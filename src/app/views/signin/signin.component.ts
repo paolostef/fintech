8d6c75d7a87269f3a2d80fc85a5fbe6e4b49dc8a
pstefani <paolo.stefani@p-lab.it>
2021-11-02 20:25:45 +0100

    sigin

--- src/app/views/signin/signin.component.ts
+++ src/app/views/signin/signin.component.ts
@@ -5,18 +5,16 @@ import { Signin } from 'src/app/models/signin';
 @Component({
   selector: 'app-signin',
   templateUrl: './signin.component.html',
-  styleUrls: ['./signin.component.scss']
+  styleUrls: ['./signin.component.scss'],
 })
 export class SigninComponent implements OnInit {
+  hide = true;
 
+  constructor(public matcher: ErrorStateMatcher) {}
 
-  constructor(public matcher: ErrorStateMatcher) { }
-
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
   singin(data: Signin) {
     console.log('signin', data);
   }
-
 }
